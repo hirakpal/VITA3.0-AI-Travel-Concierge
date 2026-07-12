21f35ce19f5350ceadf9fa7bafec623b75b97925
hirakpal <60995278+hirakpal@users.noreply.github.com>
2026-07-12 09:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+from config.constants import APP_TITLE
+
+st.set_page_config(
+    page_title=APP_TITLE,
+    layout="wide"
+)
+
+st.title(APP_TITLE)
+
+st.info(
+    "🚧 VITA 3.0 is being built step-by-step."
+)
