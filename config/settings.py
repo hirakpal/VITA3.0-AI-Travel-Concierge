96a0f5d00ffd11854f4000ec663f8f13b356ebf1
hirakpal <60995278+hirakpal@users.noreply.github.com>
2026-07-12 09:11:00 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,22 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+
+class Settings:
+
+    APP_NAME = "VITA 2.0"
+
+    VERSION = "2.0"
+
+    GEMINI_MODEL = "gemini-2.5-pro"
+
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+    GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+
+    DEBUG = True
+
+
+settings = Settings()
