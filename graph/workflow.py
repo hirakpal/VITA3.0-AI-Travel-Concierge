1adfb769fc1e3ef29155eb81ab8c4d5c74f0adb3
hirakpal <60995278+hirakpal@users.noreply.github.com>
2026-07-12 10:48:40 +0530

    Update workflow.py

--- graph/workflow.py
+++ graph/workflow.py
@@ -1,1 +1,26 @@
+from graph.graph_builder import graph
+from services.memory_service import memory_service
 
+
+class Workflow:
+
+    def run(
+        self,
+        session_id: str,
+        message: str
+    ):
+
+        state = memory_service.start_session(
+            session_id
+        )
+
+        state.user_input = message
+
+        result = graph.invoke(state)
+
+        memory_service.save_state(result)
+
+        return result
+
+
+workflow = Workflow()
